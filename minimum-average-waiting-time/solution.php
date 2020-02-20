aa9871ef611d6583e11c9728a638482239fbaefd
ivashchenko <d.ivashchenko@devcaz.com>
2020-02-20 10:02:26 +0100

    game of two stacks

---
+++ minimum-average-waiting-time/solution.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+/*
+ * Complete the minimumAverage function below.
+ */
+function minimumAverage($customers) {
+    /*
+     * Write your code here.
+     */
+    $minHeap = new SplMinHeap();
+    [$start, $length] = array_shift($customers);
+    
+//    foreach ($customers as $customer) {
+//        if ($customer[])
+//    }
+}
+
+
+
+$input = [
+    [0, 3],
+    [1, 9],
+    [2, 6],
+];
+
+minimumAverage($input);
